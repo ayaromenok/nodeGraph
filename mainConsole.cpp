0f18c04f297ccbcde628968683b7271893ee54d5
Andrey Yaromenok <ayaromenok@gmail.com>
2018-03-17 11:26:18 +0100

    #1 remove unused GIT_VERSION

--- mainConsole.cpp
+++ mainConsole.cpp
@@ -7,9 +7,7 @@ int main(int argc, char *argv[])
     QCoreApplication app(argc, argv);
 
     qDebug() << "nodeGraph/Console";
-#ifdef GIT_VERSION
-    qDebug() << GIT_VERSION;
-#endif //GIT_VERSION
+
     app.setOrganizationName("Andrey Yaromenok");
     app.setOrganizationDomain("yaromenok.info");
     app.setApplicationName(QFileInfo(app.applicationFilePath()).baseName());
