5d63835732b913a2b1b5f874daa390f4ee15f8b7
Andrey Yaromenok <ayaromenok@gmail.com>
2018-03-20 20:39:41 +0100

    #5 added separate setters/getters for YTransform

--- test/ytestnodes.cpp
+++ test/ytestnodes.cpp
@@ -34,23 +34,30 @@ YTestNodes::_addAllNodes()
         _ndl->append(new YNode("root"));//at 0 is alwyas root node
         _ndl->append(new YTransform("front", _ndl->at(0)));
         ((YTransform*)_ndl->last())->setTx(0.314);
-        ((YTransform*)_ndl->last())->setTy(3.14);
-        ((YTransform*)_ndl->last())->setTz(-31.415);
-        ((YTransform*)_ndl->last())->setR(double3{30.0,45.0,60.0});
-        ((YTransform*)_ndl->last())->setS(double3{1.1,2.2,3.3});
-        _ndl->append(new YCamera("camera", _ndl->last()));
+        ((YTransform*)_ndl->last())->setTy(-3.14);
+        ((YTransform*)_ndl->last())->setTz(31.415);
+        ((YTransform*)_ndl->last())->setR(double3{30.0,-45.0,60.0});
+        ((YTransform*)_ndl->last())->setS(double3{1.1,-2.2,3.3});
 
+        _ndl->append(new YCamera("camera", _ndl->last()));
         _ndl->append(new YTransform("MeshObj0", _ndl->at(0)));
-        ((YTransform*)_ndl->last())->setS(double3{0.9,1.8,2.7});
+        ((YTransform*)_ndl->last())->setT(double3{2.98, -3.97, 4.96});
+        ((YTransform*)_ndl->last())->setSx(0.9);
+        ((YTransform*)_ndl->last())->setSy(-1.8);
+        ((YTransform*)_ndl->last())->setSz(2.7);
+
         _ndl->append(new YMesh("Mesh0", _ndl->last()));
         _ndl->append(new YPolyCube("Cube0", _ndl->last()));
 
         _ndl->append(new YTransform("MeshObj1", _ndl->at(0)));
-        ((YTransform*)_ndl->last())->setR(double3{11.1,22.2,33.3});
+        ((YTransform*)_ndl->last())->setRx(11.1);
+        ((YTransform*)_ndl->last())->setRy(-22.2);
+        ((YTransform*)_ndl->last())->setRz(33.3);
+
         _ndl->append(new YMesh("Mesh1", _ndl->last()));
         _ndl->append(new YPolyPlane("Cube1", _ndl->last()));
 
-        _ndl->append(new YScript("Script0", _ndl->last()));
+        _ndl->append(new YScript("Script0", _ndl->at(0)));
     }
     qDebug() << "added nodes" << _ndl->length();
 
